bfa4aa58fc9d6b25645ed520f13725fc6effa45f
MonkeyWidget <github@ghosthouse.com>
2013-05-19 20:08:00 -0700

    more test cases; initialize still unvalidated and throws error

--- ruby/marsrover/app/models/route_map.rb
+++ ruby/marsrover/app/models/route_map.rb
@@ -1,2 +1,6 @@
 =begin
+
+The map has no negative coordinates
+All coordinates are integers and start at 0
+
 =end
--- ruby/marsrover/spec/unit/rover_spec.rb
+++ ruby/marsrover/spec/unit/rover_spec.rb
@@ -3,12 +3,35 @@ require 'rover'
 
 describe Rover do
   before do
-    @rover = Rover.new(1,1,'N')
+    @rover = Rover.new(1,2,'N')
   end
 
-  it "should reject a new Rover with invalid coordinates" do
-    Hash.new.should == {}
+  it "should allow a new Rover with valid coordinates and direction" do
+    Rover.new(1,1,'N').should_not raise_error
   end
+
+  it "should reject a new Rover with invalid negative coordinates" do
+    Rover.new(1,-1,'N').should raise_error
+  end
+
+  it "should reject a new Rover with invalid non-integer coordinates" do
+    Rover.new(1,1.0,'N').should raise_error
+  end
+
+  it "should reject a new Rover with invalid direction" do
+    Rover.new(1,1,'g').should raise_error
+  end
+
+
+  it "should return the current coordinates" do
+    @rover.x_location.should be 1
+    @rover.y_location.should be 2
+  end
+
+  it "should return the current direction it's facing'" do
+    @rover.facing_direction.should eql 'N'
+  end
+
 
 =begin
 
