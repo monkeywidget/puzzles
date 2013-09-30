1be85b20287dc48fdc279e8685d08b7b4c5e9d65
MonkeyWidget <github@ghosthouse.com>
2013-09-30 12:45:39 -0700

    two new (unimplemented) puzzles in python

---
+++ python/deck_boards.py
@@ -0,0 +1,14 @@
+"""
+slats on a deck:
+needed: 6 slats that are 9 feet long
+3 slats that are 5 feet long
+3 slats that are 6 feet long
+available:
+8 foot for $10
+12 foot for $15  (was this right?)
+16 foot for $18
+Problem: minimize cost.  What boards should we buy?
+one solution: knapsack
+one solution: greedy.  Is there a local maxima where this would fail?
+analysis: you can split into 3 - are there cases where this is a bad idea?
+"""
---
+++ python/first_letter_that_is_twice.py
@@ -0,0 +1,5 @@
+"""
+Given a string,
+find the first letter that appears 
+which appears exactly twice in the entire string
+"""
