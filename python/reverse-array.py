e58bb38e0aa7eb3280291a20b420a899ebae8a69
Monkey Widget <monkeywidget@users.noreply.github.com>
2019-12-10 11:00:10 -0800

    update the python array reverse for readability

--- python/reverse-array.py
+++ python/reverse-array.py
@@ -3,55 +3,58 @@
 # - print array to stdout
 
 # restrictions:
-# - in any language
+# - in any language (this is the python version)
 # - do not use "reverse" library calls
 
 # written for:
 #  speed of implementation (in the interview)
 #  speed of execution (traverse half array)
-#  novelty ! uses only "x" as a scoped/temp variable in the for loop  :)
-#  I picked python because it can easily be read in an IM window, 
-#    and requires less memorized API (vs Java)
-
-# cons:
-#  three array writes, vs two for a more basic implementation (copy to a temp variable)
-#  float to int conversion is spendy, as is div operation
-#  triple XOR trick is not real pretty looking
-#  assumes len(array) is fast
 
 # analysis:
-# in the odd-numbered array case, the int division operation gives the previous element
-# so the "middle" element is left untouched (as desired).
+# - assumes len(array) is fast!
+# - in the odd-numbered array case, the int division operation gives the previous element
+#   ...so the "middle" element is left untouched (as desired).
 
-# possible extra credit (and fun?):
-#  could condense "len(array) - 1 - x"
-#  could condense "array[?] = len(array) - 1 - x" e.g. as a lambda
-# 
-# optimise instead for brevity
-# make into a one-liner
-
-def debug_print_array(label, array):
-    for x in array:
-        print label + ": a[" + str(x) + "] = " + str(array[x])
-
-
-def main():
+# logic is easier to follow in the simple implementation
+def simple():
 
     array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-#    array = [0, 1, 2, 3, 4, 5, 6, 7, 8 ]
 
-    debug_print_array("INPUT", array)
+    print array
 
     for x in range (0, len(array)/2):
-        # print "evaluating " + str(x) + ", " + str(len(array) - 1 - x)
+        new_index = len(array) - 1 - x # for readability only
+        move_me = array[x]
+
+        array[x] = array[new_index]
+        array[new_index] = move_me
+
+    print array
+
+#  XOR: Just as a crazy restriction: What if we have only the index as a variable??
+#   three array writes, vs two for a more basic implementation (copy to a temp variable)
+#   float to int conversion is spendy, as is div operation
+#   triple XOR trick is not real pretty looking
+def xor():
+
+    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+    print array
+
+    # for extra credit: condense "array[?] = len(array) - 1 - x" e.g. as a lambda
+    for x in range (0, len(array)/2):
         array[x]                  = array[x] ^ array[len(array) - 1 - x]
         array[len(array) - 1 - x] = array[x] ^ array[len(array) - 1 - x]
         array[x]                  = array[x] ^ array[len(array)-1-x]
 
-    debug_print_array("\t\tOUTPUT", array)
+    print array
+    
+def main():
+    print "simple version:"
+    simple()
 
-
+    print "XOR version:"
+    xor()
+    
 if __name__ == "__main__":
     main()
-
-
